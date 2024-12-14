883efffc7e892f95eced9f68587b7ed14024f90f
MehdizadeMilad <milad.mehdizade1394@gmail.com>
2024-12-14 20:35:49 +0000

    chore(buyEggs): add e2e tests

--- test/e2e/CryptoAnts.t.sol
+++ test/e2e/CryptoAnts.t.sol
@@ -20,7 +20,68 @@ contract E2ECryptoAnts is Test, TestUtils {
     _eggs = new Egg(address(_cryptoAnts));
   }
 
-  function testOnlyAllowCryptoAntsToMintEggs() public {}
+  function test__BuyEggs_Only_CryptoAnt_Can_Mint_Egg() public {
+    address __nonCryptoAntsAddress = makeAddr('nonCryptoAnts');
+    vm.startPrank(__nonCryptoAntsAddress);
+
+    vm.expectRevert('Only CryptoAnts can mint eggs');
+    _eggs.mint(__nonCryptoAntsAddress, 1);
+
+    vm.deal(__nonCryptoAntsAddress, 1 ether);
+    uint8 __amountOfEggsToBought = 100;
+    _cryptoAnts.buyEggs{value: 1 ether}(__amountOfEggsToBought);
+    vm.stopPrank();
+
+    assertEq(_eggs.totalSupply(), __amountOfEggsToBought, 'Egg count should be 1 after minting from CryptoAnts');
+
+    uint256 __balanceOfBuyer = _eggs.balanceOf(__nonCryptoAntsAddress);
+    assertEq(__balanceOfBuyer, __amountOfEggsToBought);
+  }
+
+  function test__BuyEggs_Should_Emit_Events() public {
+    address __randomBuyerAddress = makeAddr('randomBuyerAddress');
+
+    vm.startPrank(__randomBuyerAddress);
+
+    vm.deal(__randomBuyerAddress, 1 ether);
+    uint8 __amountOfEggsToBought = 100;
+    uint256 __amountOfETH = 1 ether;
+
+    vm.expectEmit(true, false, false, true);
+    emit ICryptoAnts.EggsBought(__randomBuyerAddress, __amountOfEggsToBought);
+
+    _cryptoAnts.buyEggs{value: __amountOfETH}(__amountOfEggsToBought);
+
+    vm.stopPrank();
+  }
+
+  function test__BuyEggs_Validate_Buyer_Balance() public {
+    address lowBalanceAccount = makeAddr('lowBalanceAccount');
+    uint8 __amountOfEggsToBuy = 100;
+    deal(lowBalanceAccount, 0.9 ether); // not enough to buy 100 Egg.
+    vm.startPrank(lowBalanceAccount);
+
+    vm.expectRevert(ICryptoAnts.WrongEtherSent.selector);
+    _cryptoAnts.buyEggs(__amountOfEggsToBuy);
+    vm.stopPrank();
+  }
+
+  function test__BuyEggs_Validate_Egg_Supply() public {
+    address __randomBuyerAccount = makeAddr('__randomBuyerAccount');
+    uint8 __amountOfExpectedEggsToBuy = 100;
+    uint256 __amountOfETH = 1 ether;
+    deal(__randomBuyerAccount, __amountOfETH);
+
+    vm.startPrank(__randomBuyerAccount);
+    _cryptoAnts.buyEggs{value: __amountOfETH}(__amountOfExpectedEggsToBuy);
+    vm.stopPrank();
+
+    assertEq(_eggs.balanceOf(address(_cryptoAnts)), 0);
+    assertEq(_eggs.balanceOf(address(__randomBuyerAccount)), __amountOfExpectedEggsToBuy);
+    assertEq(_eggs.balanceOf(address(__randomBuyerAccount)), _eggs.totalSupply());
+    assertEq(address(_cryptoAnts).balance, __amountOfETH);
+  }
+
   function testBuyAnEggAndCreateNewAnt() public {}
   function testSendFundsToTheUserWhoSellsAnts() public {}
   function testBurnTheAntAfterTheUserSellsIt() public {}
